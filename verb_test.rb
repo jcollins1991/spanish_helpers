79b712566546ca4a48848b3104f6cd10a50eb696
Jonathan Collins <jcollins1991@gmail.com>
2012-10-03 17:28:21 -0400

    Breaks definitions out into their own files Changes gets to stdin gets, uses ARGV otherwise ;(...

--- verb_test.rb
+++ verb_test.rb
@@ -3,79 +3,27 @@
 
 # leave utf-8 header for the ñ's, á's, and all their friends
 
-verbs = {'bailar' => 'to dance',
- 'buscar' => 'to look for',
- 'caminar' => 'to walk',
- 'cantar' => 'to sing',
- 'comprar' => 'to buy',
- 'contestar' => 'to answer',
- 'conversar' => 'to talk',
- 'descansar' => 'to rest',
- 'desear' => 'to want',
- 'dibujar' => 'to draw',
- 'enseñar' => 'to teach',
- 'escuchar' => 'to listen',
- 'esperar' => 'to wait',
- 'estudiar' => 'to study',
- 'explicar' => 'to explain',
- 'hablar' => 'to talk',
- 'llegar' => 'to arrive',
- 'llevar' => 'to carry',
- 'mirar' => 'to watch',
- 'necesitar' => 'to need',
- 'practicar' => 'to practice',
- 'preguntar' => 'to ask',
- 'preparar' => 'to prepare',
- 'regresar' => 'to return',
- 'terminar' => 'to end',
- 'tomar' => 'to take',
- 'trabajar' => 'to work',
- 'viajar' => 'to travel'}
+if ARGV.length != 1
+  puts "Requires one argument: definition filename"
+  exit
+end
 
-la_familia = {'el esposo'=>'husband',
- 'la esposa'=>'wife',
- 'el hermanastro'=>'stepbrother',
- 'la hermanastra'=>'stepsister',
- 'el hermano'=>'brother',
- 'la hermana'=>'sister',
- 'el hijastro'=>'stepson',
- 'la hijastra'=>'stepdaughter',
- 'la madrastra'=>'stepmother',
- 'el padrastro'=>'stepfather',
- 'el medio hermano'=>'half brother',
- 'el media hermana'=>'half sister',
- 'los padres'=>'parents',
- 'el abuelo'=>'grandfather',
- 'la abuela'=>'grandmother',
- 'el padre'=>'father',
- 'la madre'=>'mother',
- 'los hijos'=>'children',
- 'el hijo'=>'son',
- 'la hija'=>'daughter',
- 'el cuñado'=>'brother in law',
- 'la cuñada'=>'sister in law',
- 'el nieto'=>'grandson',
- 'la nieta'=>'granddaughter',
- 'la nuera'=>'daughter in law',
- 'el yerno'=>'son in law',
- 'los parientes'=>'relatives',
- 'el primo'=>'cousin',
- 'la prima'=>'cousin',
- 'el sobrino'=>'nephew',
- 'la sobrina'=>'niece',
- 'el sugro'=>'father in law',
- 'la suegra'=>'mother in law',
- 'el tío'=>'uncle',
- 'el tía'=>'aunt'
-}
+filename = ARGV.first
+data = {}
 
+File.open(filename) do |f|
+  f.read.each_line.map do |line|
+    line = line.strip.split /,/
+    data[line[0]] = line[1]
+  end
+end
 
 def test words
   while true do
     random_word = words.keys.sample
     puts random_word
     
-    input = gets.strip
+    input = $stdin.gets.strip
     
     if input == words[random_word]
       puts "  Good job!"
@@ -87,4 +35,4 @@ def test words
   end
 end
 
-test verbs
+test data
